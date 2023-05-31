d682e3ae89e6b6d33e5cdfe178b459ddaef0c0f4
keeran-npn <kiranneupane128@gmail.com>
2023-05-31 12:28:29 +0545

    resolved provider error

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -6,11 +6,23 @@ use App\Repository\BaseRepository;
 use App\Repository\CommentRepository;
 use App\Repository\Interfaces\CommentRepositoryInterface;
 use App\Repository\Interfaces\EloquentRepositoryInterface;
+use Illuminate\Support\ServiceProvider;
 
-class RepositoryServiceProvider
+class RepositoryServiceProvider extends ServiceProvider
 {
+    /**
+     * Register services.
+     */
     public function register() {
         $this->app->bind(EloquentRepositoryInterface::class, BaseRepository::class);
         $this->app->bind(CommentRepositoryInterface::class, CommentRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
     }
 }
